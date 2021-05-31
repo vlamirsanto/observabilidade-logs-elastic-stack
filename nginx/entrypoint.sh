ac996daaba6e6c682b6dcc5dcb5f7a9cebdb8678
Vlamir Santo <vlamir.santo@dotlib.com.br>
2021-05-31 08:30:03 -0300

    [feat]: Instalação do metricbeat.

--- nginx/entrypoint.sh
+++ nginx/entrypoint.sh
@@ -1,6 +1,12 @@
 #!/bin/bash
 
 filebeat modules enable nginx
+metricbeat modules enable nginx
+
 filebeat setup
+metricbeat setup
+
 service filebeat start
+service metricbeat start
+
 nginx -g 'daemon off;'
